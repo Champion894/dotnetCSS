4ea6bb8770a2feed63e5d448f7b2288ca03626b1
TU\champ894 <thossaporn894@gmail.com>
2025-03-24 11:42:33 +0700

    commet

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResponsiveBootstrapApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This is an about page.";
+            return View();
+        }
+
+        public IActionResult Table()
+        {
+            return View();
+        }
+    }
+}
